4ae75f37888f040344dcd63bae9c8e8f7aaf3042
elianemaalouf <eliane.malouf@gmail.com>
2025-05-19 19:05:51 +0200

    formatting

--- xp_config.py
+++ xp_config.py
@@ -37,24 +37,75 @@ def save_config(config_dict, path):
         json.dump(config_dict, f, indent=4)
 
 # new experiment to add
-xp_name = "det_preds_reg_n500"
+xp_name = "prob_preds_reg_n500_cv5_es1"
+probabilistic = True
+run_validations = True # run validations or inversions
+
+root_folder = "probabilistic_preds" if probabilistic else "deterministic_preds"
+prob_sample_size = 500 if probabilistic else 1
+val_subset_size = 50 if probabilistic else 2000
+assess_training_metrics = False if probabilistic else True
+
 xp_config = {
     "xp_name": xp_name,
-    "xp_folder": f"{BASE_DIR}/Experiments/deterministic_preds/{xp_name}",
+    "xp_folder": f"{BASE_DIR}/Experiments/{root_folder}/{xp_name}",
     "train_subset": 500,
+    "val_subset_size": val_subset_size,
+    "assess_train_metrics":assess_training_metrics,
     "lambda_x_vec":[1e-4, 1e-3, 1e-2, 1e-1, 1, 10],
     "lambda_y_vec":[0, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10],
-    "run_validations": True,
-    "validations":{'types':['rmse', 'es', 'vs'],
-                   'params':[None, 2, 0.5]},
-    "probabilistic": False,
-    "prob_sample_size": 1,
+    "run_validations": run_validations,
+    "validation_repeats":5,
+    "validations":{'types':['es'],
+                   'params':[1]},
+    "probabilistic": probabilistic,
+    "prob_sample_size": prob_sample_size,
     "parameters_file": f"{BASE_DIR}/Data/parameters_matern32_Mu10_Var1p96_CorH30_CorV15_linear_81.txt",
-    "test_vecs_ids_to_invert": None,
-    "det_pred_refs":{'rmse': {'lower': None, 'center': None, 'upper': None},
-                     'es': {'lower': None, 'center': None, 'upper': None},
-                     'vs':{'lower': None, 'center': None, 'upper': None}}
+    "test_vecs_ids_to_invert": None, #[102, 106, 270, 435, 860, 154, 253, 309, 548, 966, 385, 498, 583, 608, 836, 900, 10, 18,
+    #19, 20, 1, 3, 45, 96, 140, 157, 179, 191, 204, 223, 262, 269, 283, 304, 305, 347, 363, 379, 506, 517, 521, 546, 573,
+    #607, 656, 664, 671, 680, 792, 801,],
 }
+
+vs_train_refs_filename = "reference_val_metrics_es2_vs05_rmse.pkl" if 'rmse' in xp_config['validations']['types'] else "reference_val_metrics_es1.pkl" # "reference_inv_metrics_es1_vs05_rmse.pkl" #
+
+# make det_preds_refs :
+if not probabilistic:
+    det_preds_refs = None
+else:
+    # deterministic prediction references are taken from the deterministic experiments with
+    # small noise : lambda_x = 10^-4 and lambda_y = 1
+    # large noise : lambda_x = 10^-4 and lambda_y = 10
+
+    det_preds_refs = {}
+    det_preds_refs['small_noise'] = {}
+    det_preds_refs['large_noise'] = {}
+
+    # check if 'es' is in validations types with param 2
+    if 'es' in xp_config['validations']['types']:
+        es_param = xp_config['validations']['params'][xp_config['validations']['types'].index('es')]
+        if es_param == 1:
+            # use the reference values for the probabilistic case
+            det_preds_refs['small_noise']['es'] = {'lower': 857.940, 'center': 1037.395, 'upper': 1290.326},
+            det_preds_refs['large_noise']['es'] = {'lower': 1021.957, 'center': 1195.132, 'upper': 1435.445}
+
+        if es_param == 2:
+            det_preds_refs['small_noise']['es'] = {'lower': 573.469, 'center': 819.821, 'upper': 1224.353}
+            det_preds_refs['large_noise']['es'] = {'lower': 829.229, 'center': 1109.691, 'upper': 1541.074}
+
+    # update det_preds_refs with rmse data if rmse is in validations types
+    if 'rmse' in xp_config['validations']['types']:
+        det_preds_refs['small_noise']['rmse'] = {'lower': 0.535, 'center': 0.640, 'upper': 0.782}
+        det_preds_refs['large_noise']['rmse'] = {'lower': 0.644, 'center': 0.745, 'upper': 0.878}
+
+    # update det_preds_refs with vs data if vs is in validations types
+    if 'vs' in xp_config['validations']['types']:
+        det_preds_refs['small_noise']['vs'] = {'lower': 542623.32, 'center': 697675.493, 'upper': 914168.837}
+        det_preds_refs['large_noise']['vs'] = {'lower': 710793.601, 'center': 870780.149, 'upper': 1105397.309}
+
+# update xp_config  with det_preds_refs
+xp_config['det_pred_refs'] = det_preds_refs
+xp_config['vs_train_refs_filename'] = vs_train_refs_filename
+
 save_config(xp_config, xp_config["xp_folder"])
 
 # dictionnary of all experiments
